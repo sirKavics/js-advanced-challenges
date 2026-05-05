f8266406b1a31a6ca5b0e3f504d4def1fc4d551f
sirkavics <falf555@gmail.com>
2026-05-05 18:10:01 -0400

    steps, nested loop

--- questions/steps/index.js
+++ questions/steps/index.js
@@ -18,7 +18,19 @@
  */
 
 const steps = (n) => {
-
+    
+    for (let row = 0; row < n; row++) {
+        let step = '';
+        for ( let column = 0; column < n; column++) {
+            if ( column <= row) {
+                step += '#';
+            }
+            else {
+                step += ' ';
+            }
+        }
+        console.log(step);
+    }
 };
 
 module.exports = steps;
