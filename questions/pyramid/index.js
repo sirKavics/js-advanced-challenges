57ff2c9a293713a5702db0eb3ad67812e0be2277
sirkavics <falf555@gmail.com>
2026-05-06 23:07:09 -0400

    pyramid, nested loop

--- questions/pyramid/index.js
+++ questions/pyramid/index.js
@@ -18,7 +18,22 @@
  */
 
 const pyramid = (n) => {
-  
+    const columnWidth = 2 * n - 1;
+    const middle = Math.floor(columnWidth / 2);
+
+    for (let row = 0; row < n; row++) {
+        let step = '';
+
+        for (let column = 0; column < columnWidth; column++) {
+            if (column >= middle - row && column <= middle + row) {
+                step += '#';
+            }
+            else {
+                step += ' ';
+            }
+        }
+        console.log(step);
+    }
 };
 
 module.exports = pyramid;
--- questions/steps/index.js
+++ questions/steps/index.js
@@ -21,7 +21,7 @@ const steps = (n) => {
     
     for (let row = 0; row < n; row++) {
         let step = '';
-        for ( let column = 0; column < n; column++) {
+        for (let column = 0; column < n; column++) {
             if ( column <= row) {
                 step += '#';
             }
